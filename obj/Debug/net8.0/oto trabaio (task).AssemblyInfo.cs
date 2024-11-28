e1e8603dd6f07b89c99ce1f649fc5c55c054d027
Aluno <joaopedrogeraldini123@gmail.com>
2024-11-27 21:20:11 -0300

    E a noite mudou tudo

--- obj/Debug/net8.0/oto trabaio (task).AssemblyInfo.cs
+++ obj/Debug/net8.0/oto trabaio (task).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oto trabaio (task)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2896e52f26d52381a390185b12bc71702c930d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15401302174c4c845e8fb8b676271fbbbf4c1d02")]
 [assembly: System.Reflection.AssemblyProductAttribute("oto trabaio (task)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oto trabaio (task)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
